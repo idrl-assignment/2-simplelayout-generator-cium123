9674f7786b45030a1f4fd65631fcefe04d700e9e
cium123 <53029590+cium123@users.noreply.github.com>
2020-11-17 22:12:41 +0800

    Update utils.py

--- simplelayout/generator/utils.py
+++ simplelayout/generator/utils.py
@@ -4,23 +4,19 @@
 
 from pathlib import Path
 import matplotlib.pyplot as plt
-from scipy.io import savemat
-import os
+import scipy.io as sio
 
 
 def save_matrix(matrix, file_name):
-    # TODO: 存储 matrix 到 file_name.mat, mdict 的 key 为 "matrix"
     mdict = {"matrix": matrix}
-    savemat("file_name.mat", mdict)
+    sio.savemat(file_name + ".mat", mdict)
 
 
 def save_fig(matrix, file_name):
-    # TODO: 将 matrix 画图保存到 file_name.jpg
     plt.imshow(matrix)
-    plt.savefig(filename)
+    plt.savefig(file_name+'.jpg')
 
 
 def make_dir(outdir):
-    # TODO: 当目录 outdir 不存在时创建目录
-    if not os.path.exists(outdir):
-        os.makedirs(outdir)
+    path = Path(outdir)
+    path.mkdir(parents=True, exist_ok=True)
