2e54a755bd9ec74e16daaba225d36fe0323ae0a5
cium123 <53029590+cium123@users.noreply.github.com>
2020-11-17 22:09:28 +0800

    Update __main__.py

--- simplelayout/__main__.py
+++ simplelayout/__main__.py
@@ -1,15 +1,18 @@
-from simplelayout.cli import get_options
-import scipy.io
-import scipy
-from simplelayout.generator import generate_matrix, save_matrix, save_fig
+# TODO 正确导入函数 generate_matrix, save_matrix, save_fig
+from simplelayout.cli import get_options  # TODO: 保证不修改本行也可以正确导入
+from simplelayout.generator.utils import save_matrix, save_fig, make_dir
+from simplelayout.generator.core import generate_matrix
 
 
 def main():
+    # TODO 使用导入的函数按命令行参数生成数据，包括 mat 文件与 jpg 文件
     args = get_options()
-    matrix = generate_matrix(args.board_grid, args.unit_grid, args.unit_n, args.positions)
-    mdict = {"matrix": matrix}
-    scipy.io.savemat("file_name.mat", mdict)
-    scipy.misc.imsave("file_name.jpg", matrix)
+    matrix = generate_matrix(
+        args.board_grid, args.unit_grid, args.unit_n, args.positions)
+    make_dir(args.outdir)
+    path = args.outdir + '/'
+    save_matrix(matrix, path + args.file_name)
+    save_fig(matrix, path + args.file_name)
 
 
 if __name__ == "__main__":
