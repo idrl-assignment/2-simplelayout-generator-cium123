babcb3a18776741f0fd751b224b2001acb965892
cium123 <53029590+cium123@users.noreply.github.com>
2020-11-17 19:43:47 +0800

    Update __main__.py

--- simplelayout/__main__.py
+++ simplelayout/__main__.py
@@ -1,9 +1,15 @@
-# TODO 正确导入函数 generate_matrix, save_matrix, save_fig
-from simplelayout.cli import get_options  # TODO: 保证不修改本行也可以正确导入
+from simplelayout.cli import get_options
+import scipy.io
+import scipy
+from simplelayout.generator import generate_matrix, save_matrix, save_fig
 
 
 def main():
-    raise NotImplementedError  # TODO 使用导入的函数按命令行参数生成数据，包括 mat 文件与 jpg 文件
+    args = get_options()
+    matrix = generate_matrix(args.board_grid, args.unit_grid, args.unit_n, args.positions)
+    mdict = {"matrix": matrix}
+    scipy.io.savemat("file_name.mat", mdict)
+    scipy.misc.imsave("file_name.jpg", matrix)
 
 
 if __name__ == "__main__":
